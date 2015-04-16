954cf62153749ce98195eacb870b2b9ea382f806
inoue <hirofi@hotmail.com>
2015-04-16 13:49:13 +0900

    修正

---
+++ demo01/Assets/Classes/main/controller/DownloadController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DownloadController /* : MonoBehaviour */ {
+
+
+	private string m_file_name;
+	public string FileName {
+		get { return m_file_name; }
+		set { m_file_name = value;	}
+	}
+
+	private string m_domain_name;
+	public string DomainName {
+		get { return m_domain_name; }
+		set { m_domain_name = value;	}
+	}
+/*
+	void Awake()
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+*/
+	private ContentsDownloadApiModel m_downloadmodel;
+	public void StartDownload()
+	{
+
+		try
+		{
+			// ContentsDownloadApiModelクラスは MonoBehaviour を継承しているので
+			// new ではなく GameObject に AddComponet して生成する。。。 unity お作法により 
+			GameObject emptyGameObject = new GameObject();
+			m_downloadmodel = emptyGameObject.AddComponent<ContentsDownloadApiModel> ();
+
+			m_downloadmodel.GetAssetBandle ( this.m_file_name );
+		}
+		catch
+		{
+			Debug.Log("catch");
+		}
+		finally
+		{
+			OnDownloadCompleate ();
+		}
+
+	}
+
+	// ダウンロードが終了したいらイベントマネージャに通知(イベントキュー)
+	private void OnDownloadCompleate()
+	{
+		EventManager.Instance.QueueEvent ( new DownloadEventController(this.m_file_name) );
+	}
+
+}
--- demo01/Assets/Classes/main/model/ContentsDownloadApiModel.cs
+++ demo01/Assets/Classes/main/model/ContentsDownloadApiModel.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using System.IO;
 
 public class ContentsDownloadApiModel : MonoBehaviour {
 
@@ -16,6 +17,11 @@ public class ContentsDownloadApiModel : MonoBehaviour {
 
 	string m_url = "pshpz01.isl.gacha.fujitv.co.jp/unity/";
 
+	/// <summary>
+	/// アセットをダウンロードする。
+	/// 既にキャッシュがあればそちらから取り出す。
+	/// </summary>
+	/// <param name="aDownloadFileName">A download file name.</param>
 	public void GetAssetBandle( string aDownloadFileName )
 	{
 
@@ -63,5 +69,39 @@ public class ContentsDownloadApiModel : MonoBehaviour {
 		Debug.Log(Caching.IsVersionCached(url, 1));
 		Debug.Log("DownloadAndCache end");
 	}
+
+/*
+	private IEnumerator DownloadStreamAndCache(string assetName, string url, int version = 1)
+	{
+		string txtBuffer = "";
+		string textFileName = "test.txt";
+		string path = "";
+
+#if   UNITY_ANDROID && !UNITY_EDITOR
+		string url = this.m_url + aDownloadFileName + ".unity3d.android.unity3d";
+#elif UNITY_IPHONE  && !UNITY_EDITOR
+		string url = this.m_url + aDownloadFileName + ".unity3d.iphone.unity3d";
+#else
+		string url = "http://" + this.m_url + aDownloadFileName + ".unity3d.unity3d?dl=1";
+#endif
+
+
+		#if UNITY_EDITOR
+		path = Application.streamingAssetsPath + "\\" + textFileName;
+		FileStream file = new FileStream(path,FileMode.Open,FileAccess.Read);
+		txtReader = new StreamReader(file);
+		yield return new WaitForSeconds(0f);
+		#elif UNITY_ANDROID
+		path = "jar:file://" + Application.dataPath + "!/assets" + "/" + textFileName;
+		WWW www = new WWW(path);
+		yield return www;
+		txtReader = new StringReader(www.text);
+		#endif
+		while((txtBuffer = txtReader.ReadLine()) != null){
+			description = description + txtBuffer + "\r\n";
+		}
+		label.text = description;
+	}
+*/
 
 }
